b91b5594daefd6e2f14f65ffe6c28211c3ef1fbb
nixen97 <37103694+nixen97@users.noreply.github.com>
2020-12-18 16:08:49 +0100

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,18 +1,39 @@
 project "libpqxx"
-	kind "StaticLib"
+	kind "SharedLib"
 	language "C++"
-  cppdialect "C++17"
-  staticruntime "on"
+	cppdialect "C++17"
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/stockmon")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	defines
+	{
+		"PQXX_SHARED",
+		"pqxx_EXPORTS"
+	}
 
 	files
 	{
-		"include/pqxx/*.hxx",
-    "src/*.cxx"
+		"include/pqxx/*",
+    	"src/*"
 	}
-  
+
+	links
+	{
+		"%{wks.location}/vendor/libpq/win/lib/libpq.lib",
+		"wsock32.lib",
+		"ws2_32.lib",
+		"gdi32.lib",
+		"advapi32.lib"
+	}
+
+	includedirs
+	{
+		"%{wks.location}/vendor/libpqxx/include",
+		"%{wks.location}/vendor/libpqxx/config/include",
+		"%{wks.location}/vendor/libpq/include"
+	}
+
 	filter "configurations:Debug"
 		runtime "Debug"
 		symbols "on"
